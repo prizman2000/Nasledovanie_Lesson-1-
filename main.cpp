773ba4b8977e30b60b6b4fd08effd0d0b02dfd3e
Slava Boss <prizman2000@mail.ru>
2020-03-07 11:11:25 +1000

    i like dogs

---
+++ main.cpp
@@ -0,0 +1,104 @@
+#include <iostream>
+#include <string>
+
+enum GENDER {
+    U, M, F
+};
+
+// 1. Создать несколько Human. +
+// 2. Добавить несколько дополнительных полей (не забывать добавить значимые поля в конструктор) +
+// 3. Добавить getter'ы и setter'ы +
+// 4. Продемонстрировать обращение к полям объектов дочерних и родительских классов +
+// 5. Добавить несколько наследников от различных родителей (сделать дерево из 5 классов) +
+
+class Human {
+    int age;
+    int tall;
+    bool conviction;
+    GENDER gender;
+public:
+    Human(int age, int tall, bool conviction, GENDER gender) : age(age), tall(tall), conviction(conviction) {
+        this->gender = gender;
+    }
+
+    int getAge() const {
+        return age;
+    }
+
+    void setAge(int age) {
+        this->age = age;
+    }
+
+    int getTall() const {
+        return tall;
+    }
+
+    void setTall(int tall) {
+        this->tall = tall;
+    }
+
+    bool getConviction() const {
+        return conviction;
+    }
+
+    void setConviction(bool conviction) {
+        this->conviction = conviction;
+    }
+
+    GENDER getGender() const {
+        return gender;
+    }
+
+    void setGender(GENDER gender) {
+        this->gender = gender;
+    }
+};
+
+class Student : public Human {
+    std::string numberOfZachetka;
+public:
+    Student(int age, int tall, bool conviction, GENDER gender, const std::string &numberOfZachetka) : Human(age, tall,
+                                                                                                            conviction,
+                                                                                                            gender) {
+        this->numberOfZachetka = numberOfZachetka;
+    }
+
+    const std::string &getNumberOfZachetka() const {
+        return numberOfZachetka;
+    }
+
+    void setNumberOfZachetka(const std::string &numberOfZachetka) {
+        this->numberOfZachetka = numberOfZachetka;
+    }
+};
+
+class BadStudent : public Student {
+};
+
+class GoodStudent : public Student {
+};
+
+class HardWorker : public Human {
+};
+
+int main() {
+    Human vasya(24, 173, true, GENDER::M);
+    Human slava(20, 181, false, GENDER::M);
+    Human vladimir(19, 210, false, GENDER::M);
+
+    Student olya(25, 146, false, GENDER::F, "648756568");
+
+    std::cout << vasya.getAge() << std::endl;
+    std::cout << vasya.getGender() << std::endl;
+    std::cout << vasya.getTall() << std::endl;
+    std::cout << vasya.getConviction() << std::endl;
+
+    std::cout << olya.getAge() << std::endl;
+    std::cout << olya.getGender() << std::endl;
+    std::cout << olya.getTall() << std::endl;
+    std::cout << olya.getConviction() << std::endl;
+    std::cout << olya.getNumberOfZachetka() << std::endl;
+
+
+    return 0;
+}
